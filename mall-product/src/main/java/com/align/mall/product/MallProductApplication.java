277d12b7f97c1bdb8f2c97fbc57255660408dba4
xfang <fangxin1996@gmail.com>
2022-10-27 19:12:07 -0400

    create project

---
+++ mall-product/src/main/java/com/align/mall/product/MallProductApplication.java
@@ -0,0 +1,12 @@
+package com.align.mall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallProductApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MallProductApplication.class, args);
+  }
+}
---
+++ mall-product/src/test/java/com/align/mall/product/MallProductApplicationTests.java
@@ -0,0 +1,11 @@
+package com.align.mall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProductApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
